28dbac7c8d3c04b3867ab4bbe5a86eff4c3701fb
violeine <haluudang@gmail.com>
2020-11-23 00:03:27 +0700

    add gradient and weather controll

--- web-controll/src/App.js
+++ web-controll/src/App.js
@@ -2,11 +2,10 @@ import "./App.css";
 import { useState, useEffect, useRef } from "react";
 
 const WS_URI = `ws://${window.location.hostname}:3001/ws`;
-function hexToRGB(h, brightness) {
+function hexToRGB(h) {
   let r = 0,
     g = 0,
     b = 0;
-
   // 3 digits
   if (h.length == 4) {
     r = "0x" + h[1] + h[1];
@@ -26,6 +25,9 @@ function hexToRGB(h, brightness) {
 function App() {
   const [color, setColor] = useState("#00ffff");
   const [brightness, setBrightness] = useState(64);
+  const [gradient, setGradient] = useState(["#00ffff", "#ff00ff"]);
+  const [location, setLocaton] = useState("HoChiMinh");
+  const [weather, setWeather] = useState("");
   const ws = useRef(null);
   function connectWebsocket() {
     ws.current = new WebSocket(WS_URI);
@@ -38,7 +40,25 @@ function App() {
     };
     return () => ws.current.close();
   }
-
+  const fetchFn = (payload, route) => {
+    fetch(`http://${window.location.hostname}:3001/${route}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json;charset=UTF-8",
+      },
+      body: payload,
+    });
+  };
+  const sendFn = () => fetchFn(JSON.stringify(gradient), "array");
+  const stopFn = () =>
+    fetchFn(JSON.stringify({ color, brightness: +brightness }), "stop");
+  const weatherFn = async () => {
+    const result = await fetch(
+      `https://wttr.in/${location}?format=%C`
+    ).then((res) => res.text());
+    fetchFn(JSON.stringify({ location }), "weather");
+    setWeather(result);
+  };
   useEffect(connectWebsocket, []);
   return (
     <div className="App">
@@ -51,7 +71,7 @@ function App() {
           ws.current.send(JSON.stringify(hexToRGB(e.target.value)));
         }}
       />
-      <br />
+      <hr />
       <h2>Brightness {brightness}</h2>
       <input
         type="range"
@@ -69,6 +89,43 @@ function App() {
           );
         }}
       />
+      <hr />
+      <h2>gradient</h2>
+      {gradient.map((el, idx) => {
+        return (
+          <input
+            type="color"
+            value={el}
+            onChange={(e) => {
+              const newArray = [...gradient];
+              newArray[idx] = e.target.value;
+              setGradient(newArray);
+            }}
+          />
+        );
+      })}
+      <br />
+      <button onClick={() => setGradient([...gradient, "#ffffff"])}>
+        add new gradient
+      </button>
+      <button
+        onClick={() => setGradient(gradient.slice(0, gradient.length - 1))}
+      >
+        remove gradient
+      </button>
+      <br />
+      <button onClick={sendFn}>send</button>
+      <button onClick={stopFn}> stop </button>
+      <hr />
+      <h2>weather</h2>
+      <label>location</label> <br />
+      <input
+        type="text"
+        value={location}
+        onChange={(e) => setLocaton(e.target.value)}
+      />
+      <button onClick={weatherFn}> send </button>
+      {weather ? <div>{weather}</div> : null}
     </div>
   );
 }
