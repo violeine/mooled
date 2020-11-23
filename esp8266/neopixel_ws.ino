7740b32ae7b79957ef92e8aeee6bd8386962a867
violeine <haluudang@gmail.com>
2020-11-23 19:05:33 +0700

    remove not needed stuff

--- esp8266/neopixel_ws.ino
+++ esp8266/neopixel_ws.ino
@@ -1,36 +1,41 @@
 #include <ArduinoJson.h>
 #include <ESP8266WiFi.h>
 #include <ArduinoWebsockets.h>
-const int R = 12;
-const int G = 13;
-const int B = 14;
+// // Adafruit NeoPixel library
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+ #include <avr/power.h> // Required for 16 MHz Adafruit Trinket
+#endif
+// When setting up the NeoPixel library, we tell it how many pixels,
+// and which pin to use to send signals. Note that for older NeoPixel
+// strips you might need to change the third parameter -- see the
+// strandtest example for more information on possible values.
+#define PIN  14
+#define NUMPIXELS 8
 
+int r=0;
+int g=0;
+int b=0;
+int bri=64;
+Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+// connect wifi
 const char* ssid = "violpi"; //Enter SSID
 const char* password = "0357095092"; //Enter Password
-const char* websockets_server = "192.168.4.1";
+// websocket lan
+const char* websockets_server = "gw.wlan"; // raspberry pi network
 const uint16_t port=3001; //server adress and port
 using namespace websockets;
 
-
-//LED STUFF
-//HIGH is off
-//LOW is on
-int rgb(int v){
-  return 255-v;
-}
-void setRGB(int r, int g, int b){
-  analogWrite(R,rgb(r));
-  analogWrite(G,rgb(g));
-  analogWrite(B,rgb(b));
-}
-//digital RGB 
-void dRGB(int r, int g, int b) {
-   digitalWrite(R,r?LOW:HIGH);
-   digitalWrite(G,g?LOW:HIGH);
-   digitalWrite(B,b?LOW:HIGH);  
+void showPixel() {
+  pixels.fill(pixels.Color(r,g,b));
+  pixels.setBrightness(bri);
+  pixels.show();
 }
 
-
+void statusLight(int re, int gr, int bl){
+  pixels.setPixelColor(0,re,gr,bl);
+  pixels.show();
+  }
 //WEBSOCKET 
 WebsocketsClient client; 
 //callback fn for ws event
@@ -38,16 +43,16 @@ WebsocketsClient client;
 void onEventsCallback(WebsocketsEvent event, String data) {
     if(event == WebsocketsEvent::ConnectionOpened) {
         Serial.println("Connnection Opened");
-        dRGB(1,0,1); // purple for connected
+        statusLight(255,0,255);// purple for connected
     } else if(event == WebsocketsEvent::ConnectionClosed) {
         Serial.println("Connnection Closed");
-        dRGB(1,0,0); // red for closed
+        statusLight(255,0,0);/// red for closed
     } else if(event == WebsocketsEvent::GotPing) {
         Serial.println("Got a Ping!");
-        dRGB(0,1,0); //green for ping
+       statusLight(0,255,0); //green for ping
     } else if(event == WebsocketsEvent::GotPong) {
         Serial.println("Got a Pong!");
-        dRGB(0,1,1); //cyan for pong
+        statusLight(0,255,255); //cyan for pong
     }
 }
 
@@ -58,40 +63,37 @@ void onMessageCallback(WebsocketsMessage message) {
     Serial.println(data);
     DeserializationError error = deserializeJson(doc, data);
     Serial.println(error.f_str());
-    int r=doc["red"];
-    int b=doc["blue"];
-    int g=doc["green"];
+    bri=doc["brightness"]|bri;
+    r=doc["red"]|r;
+    b=doc["blue"]|b;
+    g=doc["green"]|g;
    
-    setRGB(r,g,b);
-     
-   
-    
+    Serial.println(r);
+    Serial.println(b);
+    Serial.println(g);
+    Serial.println(bri);
+    showPixel(); 
 }
-
 void setup() {
   // put your setup code here, to run once:
-  analogWriteRange(254);    
   WiFi.mode(WIFI_STA); //station not access point
   Serial.begin(115200); //setup monitor
   WiFi.begin(ssid, password);
+    int t=0;
+    pixels.begin();
+    pixels.setBrightness(bri);
+    pixels.clear();
 
-  //set output for 3 led
-  pinMode(R,OUTPUT);
-  pinMode(G,OUTPUT);
-  pinMode(B,OUTPUT);
-  //turn off 3 led 
-  dRGB(0,0,0);
-  int t=0;
-  //attempting connect wifi
+
   WiFi.begin(ssid,password);
   while (WiFi.status() != WL_CONNECTED){
     //blinking green till connect
-     if (t%2==0) {dRGB(0,1,0);} else
-                 {dRGB(0,0,0);};
+     if (t%2==0) {statusLight(0,255,0);} else
+                 {statusLight(0,0,0);};
      t+=1;
-     delay(1000);
+     delay(500);
   }
-  dRGB(0,0,1); // blue for connected
+ statusLight(0,0,255); // blue for connected
 
   // websocket setup
    client.onEvent(onEventsCallback);
@@ -103,5 +105,8 @@ void setup() {
 }
 
 void loop() {
-  client.poll();
+  if (client.available())
+    client.poll();
+  else client.connect(websockets_server,port,"/ws");
+ 
 }
